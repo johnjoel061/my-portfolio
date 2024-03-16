b3d946547b066eba0e10912f9a527e2c6729acb4
johnjoel061 <alfabetejohnjoel@gmail.com>
2024-03-16 15:57:46 +0800

    Static Data is Okay

---
+++ staticData.js
@@ -0,0 +1,60 @@
+
+const intros = [{
+    introImage: 'Profile',
+    firstName: 'Joel',
+    caption: 'Full Stack Developer',
+    introDescription: 'Welcome to my web developer portfolio! Im Joel, a Computer Science student pursuing full-stack development in the Philippines, I immerse myself in the dynamic world of technology, combining both front-end and back-end development skills to create robust and efficient web applications. With a passion for problem-solving and innovation, I embrace the challenges of mastering various programming languages, frameworks, and tools essential for both client-side and server-side development.'
+}]
+
+const about = [{
+    aboutDescription: 'Im a Blockchain Developer with 1 year experience in building decentralized applications and contributed to a lot of projects.',
+    aboutImage: 'Image',
+    techStack:'Image'
+}]
+
+const skills = [{
+    skillTitle: 'Web Development',
+    skillDescription: 'Web app development that is decentralized',
+}]
+
+const experience = [{
+    experienceYear: '2021 - Current',
+    experienceDescription: 'A Blockchain Senior Engineer at Solana',
+}]
+
+const education = [{
+    educationYear: '2023',
+    educationDescription: 'Graduated from DEBESMSCAT with a Bachelors degree in Computer Science',
+}]
+
+const certification = [{
+    certificationTitle: 'Title',
+    certificationDescription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    certificationDate: 'April 15, 2022',
+    certificationLink: ''
+}]
+
+
+const services = [{
+    serviceIcon: '',
+    serviceTitle: 'Blockchain development',
+    serviceDescription: 'Blockchain development is the process of creating, designing, and applying blockchain technology, which is a shared, unchangeable, distributed ledger that records transactions and tracks assets within a network',
+    serviceLink: 'Learn more'
+}]
+
+const portfolio = [{
+    projectTitle: 'Project Two dfgd fgdgd',
+    projectImage: '',
+    sourceCodeLink: '',
+    liveDemoLink: '',
+}]
+
+const contact = [{
+    email: 'alfabetejohnjoel@gmail.com',
+    phoneNumber: '+63-938-0828-620',
+}]
+
+const socials = [{
+    contactIcon: '',
+    contactIconLink: '',
+}]
