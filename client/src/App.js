86ac10e6beb9c5ffd46034b7d8983d8ed05a1756
johnjoel061 <alfabetejohnjoel@gmail.com>
2024-03-21 19:57:13 +0800

    Update my backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,39 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Home from "./pages/index"
+import axios from "axios";
+import { useDispatch, useSelector } from 'react-redux';
+import { SetPortfolioData } from './redux/rootSlice';
 
 
 function App() {
+  // const [ShowLoading, setShowLoading] = useState(false);
+  const {loading, portfolioData} = useSelector(state => state.root);
+  const dispatch = useDispatch();
+
+  const getPortfolioData = async () => {
+    try {
+      const response = await axios.get('/api/portfolio/get-portfolio-data')
+      dispatch(SetPortfolioData(response.data))
+    } 
+    
+    catch (error) {
+      
+    }
+  }
+
+  useEffect(() => {
+    getPortfolioData();
+  }, [])
+
+  useEffect(() => {
+    console.log(portfolioData);
+  },[portfolioData])
+
+
   return (
     <div className="App">
+      {loading ? <loader/> : null}
       <Home/>
     </div>
   );
